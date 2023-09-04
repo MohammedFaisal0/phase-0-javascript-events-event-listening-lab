df3098d8ea0e00b9d7f7866bafa714b103172a6a
MohammedFaisal0 <alshamimohammed90@gmail.com>
2023-09-04 22:07:12 +0300

    the full lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let buttonClick = document.querySelector("#button");
 function addingEventListener() {
+  buttonClick.addEventListener("click", function(){
+    console.log("hello this is me mohammed");
+  });
 }
+
